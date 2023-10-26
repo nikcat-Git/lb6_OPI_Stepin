25fc27b1d1eff4ca43f5859903a77f39fe97883e
Степін Сергій Костянтинович <2310925@KMA.EDU>
2023-10-26 07:51:48 +0100

    Added new file c++

---
+++ lb3.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+using namespace std;
+int main()
+{
+	int num, a, b;
+	cout << "Insert number: ";
+	cin >> num;
+	a = num / 10 % 10;
+	b = num % 10;
+
+	cout << "Desatki menshe abo dorivnuyt odunicam: " << boolalpha << (a <= b) << ".\n";
+
+	(a <= b) ? cout << "Desatki menshe abo dorivnuyt edenicam." : cout << "Desatki NE menshe abo dorivnuyt edenicam.";
+
+
+}
