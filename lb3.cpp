10665b60ca0d696d54d093e7339d6fb494fee427
Степін Сергій Костянтинович <2310925@KMA.EDU>
2023-10-26 08:00:44 +0100

    Видалено ввесь код

--- lb3.cpp
+++ lb3.cpp
@@ -6,12 +6,6 @@ int main()
 	int num, a, b;
 	cout << "Insert number: ";
 	cin >> num;
-	a = num / 10 % 10;
-	b = num % 10;
-
-	cout << "Desatki menshe abo dorivnuyt odunicam: " << boolalpha << (a <= b) << ".\n";
 
 	(a <= b) ? cout << "Desatki menshe abo dorivnuyt edenicam." : cout << "Desatki NE menshe abo dorivnuyt edenicam.";
-
-
 }
